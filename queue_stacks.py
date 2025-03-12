3720c20a6afb77a4264b4e347478bd510fd9d684
Lavochcka <leva.kostetskiy13@mail.ru>
2025-03-12 17:10:36 +0300

    feat: добавил queue, состоящую из стэков

---
+++ queue_stacks.py
@@ -0,0 +1,17 @@
+from stack import Stack
+
+
+class QueueStacks:
+
+    def __init__(self, _size_l: int, _size_r: int):
+        self._size_l = _size_l
+        self._size_r = _size_r
+        self._list_l = Stack(self._size_l)
+        self._list_r = Stack(self._size_r)
+    def push(self, value):
+        self._list_l.push(value)
+    def pop(self):
+        if self._list_r.is_empty():
+            while not self._list_l.is_empty():
+                self._list_r.push(self._list_l.pop())
+        self._list_r.pop()
