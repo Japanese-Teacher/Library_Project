ea80ecd32c629f65b9f195a1edb90a4ebdce4425
Lavochcka <leva.kostetskiy13@mail.ru>
2025-03-15 22:35:27 +0300

    feat: deleted size from queue_stacks

--- queue_stacks.py
+++ queue_stacks.py
@@ -3,11 +3,9 @@ from stack import Stack
 
 class QueueStacks:
 
-    def __init__(self, _size_l: int, _size_r: int):
-        self._size_l = _size_l
-        self._size_r = _size_r
-        self._list_l = Stack(self._size_l)
-        self._list_r = Stack(self._size_r)
+    def __init__(self):
+        self._list_l = Stack()
+        self._list_r = Stack()
     def push(self, value):
         self._list_l.push(value)
     def pop(self):
