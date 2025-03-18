16a3ecb607e8017692d51721272f776804968851
Lavochcka <leva.kostetskiy13@mail.ru>
2025-03-18 21:17:28 +0300

    fix: added return to push and pop in stack

--- app/stack.py
+++ app/stack.py
@@ -7,13 +7,13 @@ class Stack:
         self._list = DynamicArray(self._size)
 
     def push(self, value):
-        self._list.append(value)
+        return self._list.append(value)
 
     def top(self):
         return self._list[self._size - 1]
 
     def pop(self):
-        self._list.pop()
+        return self._list.pop()
 
     def is_empty(self) -> bool:
         return self._list.is_empty()
