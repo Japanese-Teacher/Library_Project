73e5a69064850971efc355bc31f6289eff4a7c7c
Lavochcka <leva.kostetskiy13@mail.ru>
2025-03-18 12:57:31 +0300

    fix: added return to pop ịn stack

--- tests/test_static_array.py
+++ tests/test_static_array.py
@@ -7,3 +7,15 @@ def test_length():
     a = StaticArray(4)
     with pytest.raises(IndexError):
         _ = a[4]
+
+
+def test_size():
+    a = StaticArray(3)
+    with pytest.raises(IndexError):
+        last_index = a[3]
+
+
+def set_item():
+    a = StaticArray(2)
+    a[1] = "value"
+    assert a[1] == "value"
