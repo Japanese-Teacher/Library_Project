df2fc3b1c6ebb7cebb4f443e242732c960976ddb
Lavochcka <leva.kostetskiy13@mail.ru>
2025-03-19 14:46:13 +0300

    fix: deleted parameter in init, connected to get_size in dynamic array in stack, changed top

--- app/stack.py
+++ app/stack.py
@@ -3,20 +3,19 @@ from app.dynamic_array import DynamicArray
 
 class Stack:
     def __init__(self):
-        self._size = 0
-        self._list = DynamicArray(self._size)
+        self._list = DynamicArray(0)
 
     def push(self, value):
         return self._list.append(value)
 
     def top(self):
-        return self._list[self._size - 1]
+        return self._list[self._list.get_size() - 1]
 
     def pop(self):
         return self._list.pop()
 
     def get_size(self):
-        return self._size
+        return self._list.get_size()
 
     def is_empty(self) -> bool:
         return self._list.is_empty()
