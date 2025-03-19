0f9b2b8c7e4726934e5af054e40e0628f15c02a1
Lavochcka <leva.kostetskiy13@mail.ru>
2025-03-19 15:57:22 +0300

    feat: deleted parameter in test_size and added 0 equality verification

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -3,8 +3,8 @@ from app.stack import Stack
 
 def test_get_size():
     a = Stack()
-    expected_value = 'value'
-    a.push(expected_value)
+    assert a.get_size() == 0
+    a.push('value')
     assert a.get_size() == 1
 
 def test_push():
