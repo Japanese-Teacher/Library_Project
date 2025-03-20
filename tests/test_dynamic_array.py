556c7fa7cfdf534310ce1a5d1ef1199a56dd3d75
Lavochcka <leva.kostetskiy13@mail.ru>
2025-03-20 13:35:35 +0300

    fix: added test_get_size and annotations to dynamic_array

--- tests/test_dynamic_array.py
+++ tests/test_dynamic_array.py
@@ -1,20 +1,24 @@
 from app.dynamic_array import DynamicArray
 
 
-def test_is_empty():
+def test_is_empty() -> None:
     a = DynamicArray(4)
     a.append("value")
     assert a.is_empty() == False
 
 
-def test_append():
+def test_append() -> None:
     a = DynamicArray(1)
     a.append("value")
     assert a[1] == "value"
 
 
-def test_set_item():
+def test_set_item() -> None:
     a = DynamicArray(2)
     a[0] = "value"
     a[0] = 2
     assert a[0] == 2
+
+def test_get_size() -> None:
+    a = DynamicArray(2)
+    assert a.get_size() == 2
