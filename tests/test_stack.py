4ef7e7cdc0eb752df974518a017251057ae60c93
Lavochcka <leva.kostetskiy13@mail.ru>
2025-03-20 13:36:17 +0300

    fix: annotations to test_stack

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,25 +1,25 @@
 from app.stack import Stack
 
 
-def test_get_size():
+def test_get_size() -> None:
     a = Stack()
     assert a.get_size() == 0
     a.push('value')
     assert a.get_size() == 1
 
-def test_push():
+def test_push() -> None:
     a = Stack()
     expected_value = 'value'
     a.push(expected_value)
     assert a.top() == expected_value
 
-def test_top():
+def test_top() -> None:
     a = Stack()
     expected_value = 'value'
     a.push(expected_value)
     assert a.top() == expected_value
 
-def test_pop():
+def test_pop() -> None:
     a = Stack()
     expected_value = 'value'
     a.push(expected_value)
