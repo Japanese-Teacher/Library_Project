8ad32a5100e6125c910b9999a2898c6f7b9ff418
Lavochcka <leva.kostetskiy13@mail.ru>
2025-03-20 13:29:46 +0300

    fix: added test_top, test_pop to stack

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -12,3 +12,16 @@ def test_push():
     expected_value = 'value'
     a.push(expected_value)
     assert a.top() == expected_value
+
+def test_top():
+    a = Stack()
+    expected_value = 'value'
+    a.push(expected_value)
+    assert a.top() == expected_value
+
+def test_pop():
+    a = Stack()
+    expected_value = 'value'
+    a.push(expected_value)
+    assert a.pop() == expected_value
+    assert a.get_size() == 0
